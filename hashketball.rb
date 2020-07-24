9d2ad1b48abe2364821c8fa16a9dd22bb385488f
Ignas Butautas <ignasbutautas7@gmail.com>
2020-07-24 20:49:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,7 +15,7 @@ def game_hash
           blocks: 1,
           slam_dunks: 1
         },
-        "Reggie Evans" => {
+        {
           player_name: "Reggie Evans",
           number: 30,
           shoe: 14,
@@ -26,7 +26,7 @@ def game_hash
           blocks: 12,
           slam_dunks: 7
         },
-        "Brook Lopez" => {
+        {
           player_name: "Brook Lopez",
           number: 11,
           shoe: 17,
@@ -37,7 +37,7 @@ def game_hash
           blocks: 1,
           slam_dunks: 15
         },
-        "Mason Plumlee" => {
+        {
           player_name: "Mason Plumlee",
           number: 1,
           shoe: 19,
@@ -48,7 +48,7 @@ def game_hash
           blocks: 8,
           slam_dunks: 5
         },
-        "Jason Terry" => {
+        {
           player_name: "Jason Terry",
           number: 31,
           shoe: 15,
